1835e5589836ec1742568797e16f0cbba8341779
R.J.V. Bertin <rjvbertin@gmail.com>
2017-06-07 20:59:19 +0200

    sync with macstrop, KDE4 ports cleanup

--- kde/kdelibs4/files/kdeinit4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "${TERM}" != "" ] ;then
-	# launched attached to a terminal session; do not use LaunchServices:
-	exec @KDEAPPDIR@/kdeinit4.app/Contents/MacOS/kdeinit4 "$@"
-else
-	exec open -W -a @KDEAPPDIR@/kdeinit4.app --args "$@"
-fi
--- kde/kdevelop-git/files/detach-terminal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-exec 0>&- # close stdin
-exec 1>&- # close stdout
-exec 2>&- # close stderr
-
-exec "${0}.bin" "$@"
